dab3a5dc7d4877b29722c3cbd7ff0d826ada5bd0
tmddk9445 <tmddk9445@naver.com>
2023-01-19 09:01:11 +0900

    first commit

---
+++ src/main/java/com/mong/mmbs/MmbsApplication.java
@@ -0,0 +1,13 @@
+package com.mong.mmbs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MmbsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MmbsApplication.class, args);
+	}
+
+}
