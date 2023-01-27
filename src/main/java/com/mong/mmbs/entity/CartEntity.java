d5c8686a4376555c514eef830e33a8709040e8f4
park jun hyeon <dlsnditk90@naver.com>
2023-01-27 15:45:00 +0900

    parkjun

--- src/main/java/com/mong/mmbs/entity/CartEntity.java
+++ src/main/java/com/mong/mmbs/entity/CartEntity.java
@@ -6,7 +6,6 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Table;
 
-import com.mong.mmbs.dto.CartDto;
 import com.mong.mmbs.dto.SignUpDto;
 
 import lombok.AllArgsConstructor;
