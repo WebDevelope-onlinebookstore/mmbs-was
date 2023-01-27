65333d48859724d18e9101026fc7a8062f9ee771
park jun hyeon <dlsnditk90@naver.com>
2023-01-27 23:17:13 +0900

    park

--- src/main/java/com/mong/mmbs/controller/DtlpageController.java
+++ src/main/java/com/mong/mmbs/controller/DtlpageController.java
@@ -4,6 +4,7 @@ package com.mong.mmbs.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
