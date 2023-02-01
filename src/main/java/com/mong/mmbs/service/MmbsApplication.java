dab651818beb8f044125b3e136681478c8deed72
DESKTOP-V1J5AEQ\장민녕 <jgn159753@naver.com>
2023-02-01 14:25:37 +0900

    2023.02.01

--- src/main/java/com/mong/mmbs/service/MmbsApplication.java
+++ src/main/java/com/mong/mmbs/service/MmbsApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class BoardCrudApplication {
+public class MmbsApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(BoardCrudApplication.class, args);
+		SpringApplication.run(MmbsApplication.class, args);
 	}
 
 }
