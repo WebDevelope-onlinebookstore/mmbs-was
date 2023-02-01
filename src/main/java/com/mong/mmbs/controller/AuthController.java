b0e585c1dd0353beec0c71115395be085e137019
park jun hyeon <dlsnditk90@naver.com>
2023-02-01 12:41:08 +0900

    park

--- src/main/java/com/mong/mmbs/controller/AuthController.java
+++ src/main/java/com/mong/mmbs/controller/AuthController.java
@@ -1,8 +1,7 @@
 package com.mong.mmbs.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
-
-
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
