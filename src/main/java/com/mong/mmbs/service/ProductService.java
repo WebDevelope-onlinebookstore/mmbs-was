0a7ff9645abb9bf185caf7a0a93626f6e18b1e58
huiju1313 <ft179@naver.com>
2023-02-03 15:20:10 +0900

    ko

--- src/main/java/com/mong/mmbs/service/ProductService.java
+++ src/main/java/com/mong/mmbs/service/ProductService.java
@@ -15,31 +15,29 @@ public class ProductService {
 	
 	@Autowired ProductRepository productRepository;
 	
+	
+	
+//public ResponseDto<?> getProductAgeAll(String productAgeCategory) {
+//		
+//		List<ProductEntity> ProductAgeAll = null;
+//		// 0~3
+//		if (productAgeCategory.equals("1")) {
+//					ProductAgeAll = productRepository.findByProductAgeAll(productAgeCategory);
+//		}
+//		return ResponseDto.setSuccess("Success", ProductAgeAll);
+//		}
+	
 	//연령
 	
 	public ResponseDto<?> getProductAgeList(String productAgeCategory, String productSubAgeCategory) {
 		
 		List<ProductEntity> productAgeList = null;
-		// 0~3
-		if (productAgeCategory.equals("1")) {
-			for(Integer i = 0; i<6; i++) {
-				if
-//				(i==0) {
-//					productAgeList = productRepository.findByProductAge(productAgeCategory);
-//				}
-//			else
-				(productSubAgeCategory.equals(i.toString())) {
-					productAgeList = productRepository.findByProductAgeAndProductSubAge(productAgeCategory, productSubAgeCategory);
-				}
-			}
-		} // 4~7
-		else if (productAgeCategory.equals("2")) {
-			for(Integer i = 0; i<6; i++) {
-				if(productSubAgeCategory.equals(i.toString())) {
-					productAgeList = productRepository.findByProductAgeAndProductSubAge(productAgeCategory, productSubAgeCategory);
-				}
-			}
-		}
+		
+		if(productSubAgeCategory.equals("0"))
+			productAgeList = productRepository.findByProductAge(productAgeCategory);
+		else 
+			productAgeList = productRepository.findByProductAgeAndProductSubAge(productAgeCategory, productSubAgeCategory);
+
 		return ResponseDto.setSuccess("Success", productAgeList);
 	}
 		
@@ -49,57 +47,13 @@ public class ProductService {
 		System.out.println(productGenreCategory);
 		
 		List<ProductEntity> productGenreList = null; 
-			if (productGenreCategory.equals("국내도서")) {
-				for(Integer i = 0; i<7; i++) {
-					if(productSubGenreCategory.equals(i.toString())) {
-						productGenreList = productRepository.findByProductGenreAndProductSubGenre(productGenreCategory, productSubGenreCategory);
-					}
-				}
-			}
-			else if (productGenreCategory.equals("해외도서")) {
-				for(Integer i = 0; i<5; i++) {
-					if(productSubGenreCategory.equals(i.toString())) {
-						productGenreList = productRepository.findByProductGenreAndProductSubGenre(productGenreCategory, productSubGenreCategory);
-					}
-				}
-			}
-			else if (productGenreCategory.equals("전집")) {
-				for(Integer i = 0; i<6; i++) {
-					if(productSubGenreCategory.equals(i.toString())) {
-						productGenreList = productRepository.findByProductGenreAndProductSubGenre(productGenreCategory, productSubGenreCategory);
-					}
-				}
-			}
-			else if (productGenreCategory.equals("부모님의 서재")) {
-				for(Integer i = 0; i<5; i++) {
-					if(productSubGenreCategory.equals(i.toString())) {
-						productGenreList = productRepository.findByProductGenreAndProductSubGenre(productGenreCategory, productSubGenreCategory);
-					}
-				}
-			}
-			else if (productGenreCategory.equals("토이/놀이")) {
-				for(Integer i = 0; i<4; i++) {
-					if(productSubGenreCategory.equals(i.toString())) {
-						productGenreList = productRepository.findByProductGenreAndProductSubGenre(productGenreCategory, productSubGenreCategory);
-					}
-				}
-			}
+		
+		if(productSubGenreCategory.equals("0"))
+			productGenreList = productRepository.findByProductGenre(productGenreCategory);
+		else 
+			productGenreList = productRepository.findByProductGenreAndProductSubGenre(productGenreCategory, productSubGenreCategory);
+
 			return ResponseDto.setSuccess("Success", productGenreList);
 			}
 	
 }
-
-
-
-//나이와 장르를 합쳐서 하는 방법
-
-//public ResponseDto<?> getBookList(String productCategory, String productSubCategory) {
-//	
-//	List<ProductEntity> productList = null;
-//	if (productCategory.equals("1") || productCategory.equals("2"))
-//		productList = productRepository.findByProductAgeAndProductAgeCategory(productCategory, productSubCategory);
-//	else 
-//		productList = productRepository.findByProductGenreAndProductGenreCategory(productCategory, productSubCategory);
-//	
-//	return ResponseDto.setSuccess("Success", productList);
-//}
