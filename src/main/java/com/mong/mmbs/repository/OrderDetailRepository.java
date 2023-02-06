af762a090fdc0141eadb81b3db55d7cd24b41ab5
tmddk9445 <tmddk9445@naver.com>
2023-02-06 11:06:35 +0900

    2023026 WAS

--- src/main/java/com/mong/mmbs/repository/OrderDetailRepository.java
+++ src/main/java/com/mong/mmbs/repository/OrderDetailRepository.java
@@ -1,5 +1,7 @@
 package com.mong.mmbs.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -7,5 +9,5 @@ import com.mong.mmbs.entity.OrderDetailEntity;
 
 @Repository
 public interface OrderDetailRepository extends JpaRepository<OrderDetailEntity, Integer> {
-
-}
+  public List<OrderDetailEntity> findByOrderNumber(String orderNumber);
+} 
