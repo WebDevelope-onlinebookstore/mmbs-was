726358d1c9f910b62856f6aa0336e864a75f086d
tmddk9445 <tmddk9445@naver.com>
2023-02-13 14:10:23 +0900

    20230213 LEE

--- src/main/java/com/mong/mmbs/service/AskService.java
+++ src/main/java/com/mong/mmbs/service/AskService.java
@@ -1,6 +1,11 @@
 package com.mong.mmbs.service;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.time.Instant;
+import java.time.temporal.ChronoUnit;
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -10,6 +15,7 @@ import com.mong.mmbs.dto.ResponseDto;
 import com.mong.mmbs.dto.AskDto;
 import com.mong.mmbs.dto.AskUpdateDto;
 import com.mong.mmbs.dto.AskDeleteDto;
+import com.mong.mmbs.dto.AskSearchDto;
 import com.mong.mmbs.repository.AskRepository;
 import com.mong.mmbs.entity.AskEntity;
 
@@ -91,5 +97,30 @@ public class AskService {
 			ResponseDto.setFailed("Failed");
 		}
 		return ResponseDto.setSuccess("Success", askId);
+	}
+
+	public ResponseDto<?> askSearch (AskSearchDto dto, String userId) {
+
+		String askStatus = dto.getAskStatus();
+		int months = dto.getAskDatetime();
+		String askSort = dto.getAskSort();
+
+		SimpleDateFormat simpleDateFormat = new SimpleDateFormat("yyyy-MM-dd");
+		Date date = Date.from(Instant.now().minus(months * 30, ChronoUnit.DAYS));
+		String askDateTime = simpleDateFormat.format(date);
+
+		List<AskEntity> askList = new ArrayList<AskEntity>();
+
+		System.out.println(askDateTime);
+
+		try {
+			askList = askRepository.findByAskWriterAndAskDatetimeGreaterThanEqualAndAskSortContainsAndAskStatusContainsOrderByAskDatetimeDesc(userId, askDateTime, askSort, askStatus);
+			if (askList == null) return ResponseDto.setFailed("Does not Exist Order");
+		} catch(Exception exception){
+			exception.printStackTrace();
+			return ResponseDto.setFailed("Database Error");
+		}
+		return ResponseDto.setSuccess("Success", askList);
+	
 	}
 }
